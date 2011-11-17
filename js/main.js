73a79d8b9ad6bcb32d2d9ab7640e897d8394b78e
Matthew Butt <matthew.butt@7digital.com>
2011-11-17 17:22:54 +0000

    Wired up initial sine wave

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-var context = new webkitAudioContext();
+/*global webkitAudioContext: false */
+
+(function () {
+	"use strict";
+	var context = new webkitAudioContext(),
+		jsNode = context.createJavaScriptNode(2048, 1, 1),
+		x = 0,
+		sampleRate = context.sampleRate,
+		process = function (e) {
+			var data = e.outputBuffer.getChannelData(0),
+				i;
+			for (i = 0; i < data.length; i = i + 1) {
+				data[i] = Math.sin(x * 2 * Math.PI * 440 / sampleRate);
+				x = x + 1;
+			}
+		};
+	
+	jsNode.onaudioprocess = process;
+	
+	jsNode.connect(context.destination);
+
+}());
