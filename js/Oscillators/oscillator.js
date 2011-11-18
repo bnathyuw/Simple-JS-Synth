0a4f9c5fe10e40ca52360af9302850b526bef305
Matthew Butt <matthew.butt@7digital.com>
2011-11-18 16:00:38 +0000

    Refactoring

--- js/Oscillators/oscillator.js
+++ js/Oscillators/oscillator.js
@@ -3,10 +3,15 @@ var Oscillator = (function () {
 
 	var Oscillator = function Oscillator(spec) {
 		var currentIndex = 0,
+			waveTable = spec.waveTable,
+			frequency = spec.frequency,
+			sampleRate = spec.sampleRate,
+			amplitude = spec.amplitude,
+			waveTableSize = spec.waveTableSize,
 			next = function () {
-				var returnValue = spec.waveTable.getValue(currentIndex);
-				currentIndex = currentIndex + spec.frequency * spec.waveTableSize / spec.sampleRate;
-				return returnValue * spec.amplitude;
+				var returnValue = waveTable.getValue(currentIndex);
+				currentIndex = currentIndex + frequency * waveTableSize / sampleRate;
+				return returnValue * amplitude;
 			};
 
 		this.next = next;
