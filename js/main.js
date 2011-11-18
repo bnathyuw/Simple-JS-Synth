b08334e363c648ee26685750026a6d000aef91a1
Matthew Butt <matthew.butt@7digital.com>
2011-11-18 18:05:22 +0000

    Create a new node each time play is pressed

--- js/main.js
+++ js/main.js
@@ -1,44 +1,46 @@
 /*global AudioContext: false, webkitAudioContext: false, document: false,
+
 	SineWave: false, Oscillator: false */
 
 (function () {
 	"use strict";
 	var AC = webkitAudioContext,
 		context = new AC(),
-		jsNode = context.createJavaScriptNode(2048, 1, 1),
-		sineWave = new SineWave(),
 		x = 0,
 		frequency = 440,
-		sampleRate = context.sampleRate,
-		amplitude = new EnvelopeOscillator({
-			duration: 5,
-			sampleRate: sampleRate,
-			waveTable: new ADSRWave({
-				attackTime: 1, 
-				decayTime: 1,
-				sustainLevel: 0.5,
-				sustainTime: 1,
-				releaseTime: 1
-			}),
-			amplitude: 1
-		}),
-		oscillator = new Oscillator({
-			frequency: frequency,
-			sampleRate: sampleRate,
-			waveTable: sineWave,
-			amplitude: amplitude
-		}),
-		process = function (e) {
-			var data = e.outputBuffer.getChannelData(0),
-				i;
-			for (i = 0; i < data.length; i = i + 1) {
-				data[i] = oscillator.next();
-			}
-		};
-
-	jsNode.onaudioprocess = process;
+		sampleRate = context.sampleRate
 
 	document.getElementById("play").onclick = function () {
+		var jsNode = context.createJavaScriptNode(2048, 1, 1),
+			sineWave = new SineWave(),
+			amplitude = new EnvelopeOscillator({
+				duration: 5,
+				sampleRate: sampleRate,
+				waveTable: new ADSRWave({
+					attackTime: 1, 
+					decayTime: 1,
+					sustainLevel: 0.5,
+					sustainTime: 1,
+					releaseTime: 1
+				}),
+				amplitude: 1
+			}),
+			oscillator = new Oscillator({
+				frequency: frequency,
+				sampleRate: sampleRate,
+				waveTable: sineWave,
+				amplitude: amplitude
+			}),
+
+			process = function (e) {
+				var data = e.outputBuffer.getChannelData(0),
+					i;
+				for (i = 0; i < data.length; i = i + 1) {
+					data[i] = oscillator.next();
+				}
+			};
+
+		jsNode.onaudioprocess = process;
 		jsNode.connect(context.destination);
 	};
 
