d366bfbcf095025a7d3dcd82683dfa5c80889fe0
Matthew Butt <matthew@matthewbutt.com>
2011-11-19 16:53:41 +0000

    a bit of inheritance

--- js/WaveTables/loop-wave.js
+++ js/WaveTables/loop-wave.js
@@ -1,28 +1,12 @@
 var LoopWave = function (table) {
 	"use strict";
-
-	var getTableValue = function (index) {
+  return new WaveBase({
+		table: table,
+		getTableValue: function (index) {
 			return table[index % table.length];
 		},
-		interpolateValue = function (index) {
-			var i1 = Math.floor(index),
-				i2 = Math.ceil(index),
-				v1 = getTableValue(i1),
-				v2 = getTableValue(i2),
-				di = index % 1,
-				dv = v2 - v1;
-
-			return v1 + (dv * di);
-
-		},
-		getValue = function (index) {
-			index = index * table.length;
-			if (index % 1 === 0) {
-				return getTableValue(index);
-			} else {
-				return interpolateValue(index);
-			}
-		};
-
-	this.getValue = getValue;
+		getTableLength: function() {
+			return table.length;
+		}
+	});
 };
---
+++ js/WaveTables/wave-base.js
@@ -0,0 +1,27 @@
+var WaveBase = function (spec) {
+	"use strict";
+
+	var table = spec.table,
+		getTableValue = spec.getTableValue,
+		getTableLength = spec.getTableLength,
+		interpolateValue = function (index) {
+			var i1 = Math.floor(index),
+				i2 = Math.ceil(index),
+				v1 = getTableValue(i1),
+				v2 = getTableValue(i2),
+				di = index % 1,
+				dv = v2 - v1;
+
+			return v1 + (dv * di);
+		},
+		getValue = function (index) {
+			index = index * getTableLength();
+			if (index % 1 === 0) {
+				return getTableValue(index);
+			} else {
+				return interpolateValue(index);
+			}
+		};
+
+	this.getValue = getValue;
+};
