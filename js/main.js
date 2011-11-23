5530fda96925685f9af0dcd966125527a7795afb
Matthew Butt <matthew.butt@7digital.com>
2011-11-23 17:30:18 +0000

    Figured out why the sounds were getting shorter

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /*global AudioContext: false, webkitAudioContext: false, document: false,
 	SineWave: false, Oscillator: false, ADSRWave: false, EnvelopeWave: false,
-	EnvelopeGenerator: false, OscillatorJavaScriptNode: false */
+	EnvelopeGenerator: false, OscillatorJavaScriptNode: false, FrequencyCentrer: false,
+	OscillatorAdder: false */
 
 (function () {
 	"use strict";
@@ -19,38 +20,43 @@
 		oscillatorWave = new SineWave();
 
 	document.getElementById("play").onclick = function () {
-		var amplitude = new EnvelopeGenerator({
-				duration: 10,
-				sampleRate: sampleRate,
-				waveTable: amplitudeWave,
-				amplitude: 1
-			}),
+		var amplitude,
+			frequency,
 			compoundWave,
 			oscillators = [],
 			oscillatorNode,
 			i;
-		
-		for (i = 0; i < 10; i = i + 1) {
-			oscillators.push(new Oscillator({
-				frequency: new FrequencyCentrer({
-					wave: new Oscillator({
-						frequency: 8,
-						sampleRate: sampleRate,
-						waveTable: frequencyWave,
-						amplitude: 5
-					}),
-					frequency: 440 * (2 * i + 1)
+
+		for (i = 0; i < 20; i = i + 1) {
+			frequency = new FrequencyCentrer({
+				wave: new Oscillator({
+					frequency: 8,
+					sampleRate: sampleRate,
+					waveTable: frequencyWave,
+					amplitude: 5
 				}),
+				frequency: 440 * (2 * i + 1)
+			});
+
+			amplitude = new EnvelopeGenerator({
+				duration: 5,
+				sampleRate: sampleRate,
+				waveTable: amplitudeWave,
+				amplitude: 1
+			});
+
+			oscillators.push(new Oscillator({
+				frequency: frequency,
 				sampleRate: sampleRate,
 				waveTable: new SineWave(),
 				amplitude: amplitude
 			}));
 		}
-		
+
 		compoundWave = new OscillatorAdder({
 			oscillators: oscillators
 		});
-		
+
 		oscillatorNode = new OscillatorJavaScriptNode({
 			context: context,
 			oscillator: compoundWave
