a2336e6412ff72edfcb4f9b57f824d7c8e77f0c4
Matthew Butt <matthew.butt@7digital.com>
2011-12-01 17:07:16 +0000

    Added triangle wave

---
+++ spec/Synth/WaveTables/triangle-wave-spec.js
@@ -0,0 +1,47 @@
+/*global describe: false, it: false, expect: false, TriangleWave: false */
+
+describe("TriangleWave", function () {
+	"use strict";
+
+	var triangleWave = new TriangleWave();
+
+	it("should return 0 at position 0", function () {
+		var result = triangleWave.getValue(0);
+		expect(result.toFixed(4)).toEqual("0.0000");
+	});
+
+	it("should return 0.5 at position 0.125", function () {
+		var result = triangleWave.getValue(0.125);
+		expect(result.toFixed(4)).toEqual("0.5000");
+	});
+
+	it("should return 1 at position 0.25", function () {
+		var result = triangleWave.getValue(0.25);
+		expect(result.toFixed(4)).toEqual("1.0000");
+	});
+
+	it("should return 0.5 at position 0.375", function () {
+		var result = triangleWave.getValue(0.375);
+		expect(result.toFixed(4)).toEqual("0.5000");
+	});
+
+	it("should return 0 at position 0.5", function () {
+		var result = triangleWave.getValue(0.5);
+		expect(result.toFixed(4)).toEqual("0.0000");
+	});
+
+	it("should return -0.5 at position 0.625", function () {
+		var result = triangleWave.getValue(0.625);
+		expect(result.toFixed(4)).toEqual("-0.5000");
+	});
+
+	it("should return -1 at position 0.75", function () {
+		var result = triangleWave.getValue(0.75);
+		expect(result.toFixed(4)).toEqual("-1.0000");
+	});
+
+	it("should return -0.5 at position 0.875", function () {
+		var result = triangleWave.getValue(0.875);
+		expect(result.toFixed(4)).toEqual("-0.5000");
+	});
+});
