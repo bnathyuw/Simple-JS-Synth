95ea7a68cd401c65be8d38bc2c39fdb332659b74
Matthew Butt <matthew.butt@7digital.com>
2011-12-07 15:11:08 +0000

    Got FM workin with Modulation index

--- js/frequency-modulation.js
+++ js/frequency-modulation.js
@@ -6,10 +6,12 @@
 
 	var carrierSlider = document.getElementsByName("carrier")[0],
 		modulatorSlider = document.getElementsByName("modulator")[0],
+		modulationIndexSlider = document.getElementsByName("modulation-index")[0],
 		playButton = document.getElementsByName("play")[0],
 		stopButton = document.getElementsByName("stop")[0],
 		carrierSpan = document.createElement("span"),
 		modulatorSpan = document.createElement("span"),
+		modulationIndexSpan = document.createElement("span"),
 		AC = webkitAudioContext,
 		context = new AC(),
 		oscillatorNode,
@@ -22,12 +24,24 @@
 			return +modulatorSlider.value;
 		},
 
+		getModulationIndex = function () {
+			return +modulationIndexSlider.value;
+		},
+		
+		getModulatorAmplitude = function () {
+			return getModulator() * getModulationIndex();
+		},
+
 		updateCarrier = function () {
 			carrierSpan.innerText = getCarrier();
 		},
 
 		updateModulator = function () {
 			modulatorSpan.innerText = getModulator();
+		},
+
+		updateModulationIndex = function () {
+			modulationIndexSpan.innerText = getModulationIndex();
 		},
 
 		stop = function () {
@@ -44,7 +58,7 @@
 					frequency: new FrequencyCentrer({
 						wave: new Oscillator({
 							frequency: new CallbackGenerator({callback: getModulator}),
-							amplitude: new CallbackGenerator({callback: getModulator}),
+							amplitude: new CallbackGenerator({callback: getModulatorAmplitude}),
 							waveTable: new SineWave(),
 							sampleRate: context.sampleRate
 						}),
@@ -60,12 +74,15 @@
 
 	updateCarrier();
 	updateModulator();
+	updateModulationIndex();
 
 	carrierSlider.parentNode.appendChild(carrierSpan);
 	modulatorSlider.parentNode.appendChild(modulatorSpan);
+	modulationIndexSlider.parentNode.appendChild(modulationIndexSpan);
 
 	carrierSlider.addEventListener("change", updateCarrier);
 	modulatorSlider.addEventListener("change", updateModulator);
+	modulationIndexSlider.addEventListener("change", updateModulationIndex);
 
 	playButton.addEventListener("click", play);
 	stopButton.addEventListener("click", stop);
