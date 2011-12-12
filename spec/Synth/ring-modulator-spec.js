c970d74d34509fcbee05bc707a68890236316691
Matthew Butt <matthew.butt@7digital.com>
2011-12-12 14:52:27 +0000

    RingModulator implementation in its own file

--- spec/Synth/ring-modulator-spec.js
+++ spec/Synth/ring-modulator-spec.js
@@ -1,25 +1,3 @@
-var RingModulator = function RingModulator(spec) {
-
-	if (!this instanceof RingModulator) {
-		return new RingModulator(spec);
-	}
-
-	var carrier = spec.carrier,
-
-		modulator = new Oscillator({
-			amplitude: 1,
-			waveTable: new SineWave(),
-			frequency: spec.frequency,
-			sampleRate: spec.sampleRate		
-		}),
-
-		next = function () {
-			return carrier.next() * modulator.next();
-		};
-
-	this.next = next;
-};
-
 describe("RingModulator", function () {
 	var carrier,
 		modulatorFrequency,
